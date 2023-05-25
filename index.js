70388efa46b56756db9f8869419ae09db2e11b9d
justJayesh <115465646+justtJayesh@users.noreply.github.com>
2023-05-25 13:31:25 +0530

    dependency added

--- index.js
+++ index.js
@@ -1,16 +1,17 @@
 // importing json-server library
-const jsonServer = require("json-server")
+const jsonServer = require("json-server");
 const server = jsonServer.create();
 const router = jsonServer.router("db.json");
-const middleware = jsonServer.defaults()
-require("dotenv").config()
+const cors = require("cors");
+const middleware = jsonServer.defaults();
+require("dotenv").config();
 
-const port = process.env.port || 8000
+const port = process.env.port || 8000;
 
-server.use(middleware)
-server.use(router)
+server.use(cors());
+server.use(middleware);
+server.use(router);
 
-
-server.listen(port, ()=>{
-    console.log(`Server is running at ${port}`)
-})
+server.listen(port, () => {
+    console.log(`Server is running at ${port}`);
+});
