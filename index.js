61e5e2dc1b6e4b5e781b2fb2894d74f4387ccfa4
justJayesh <115465646+justtJayesh@users.noreply.github.com>
2023-05-25 12:26:08 +0530

    Server created

---
+++ index.js
@@ -0,0 +1,16 @@
+// importing json-server library
+const jsonServer = require("json-server")
+const server = jsonServer.create();
+const router = jsonServer.router("db.json");
+const middleware = jsonServer.defaults()
+require("dotenv").config()
+
+const port = process.env.port || 8000
+
+server.use(middleware)
+server.use(router)
+
+
+server.listen(port, ()=>{
+    console.log(`Server is running at ${port}`)
+})
