4ac00de62d356484a457017fe4dd4dfc190e522d
justJayesh <115465646+justtJayesh@users.noreply.github.com>
2023-05-26 11:28:18 +0530

    fixing cors

--- index.js
+++ index.js
@@ -2,13 +2,13 @@
 const jsonServer = require("json-server");
 const server = jsonServer.create();
 const router = jsonServer.router("db.json");
-const cors = require("cors");
+// const cors = require("cors");
 const middleware = jsonServer.defaults();
 require("dotenv").config();
 
 const port = process.env.port || 8000;
 
-server.use(cors());
+// server.use(cors());
 server.use(middleware);
 server.use(router);
 
